08b360216af0e0203ce1c04a121b25ca355f90af
郭茂 <guomao0120@gmail.com>
2016-03-25 14:18:24 +0800

    asd

---
+++ rty/rty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rty
+//
+//  Created by 郭茂 on 16/3/25.
+//  Copyright © 2016年 郭茂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
